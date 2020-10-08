ba92f24b4b3f8c2dc0b6bee7d956cb416ce0d954
Naum <naum@Naums-MacBook-Pro.local>
2020-10-08 20:31:56 +0300

    save

--- dz2t3.js
+++
@@ -1,9 +0,0 @@
-var array = [1, 1.3, 55, 66.3, 5];
-// var num = 5;
-var res = 0;
-
-for (var i=0; i<=array.length; i++){;
-    if (array[i]%5==0){
-        res++
-    }
-}console.log(res)
--- dz2t4.js
+++
@@ -1,7 +0,0 @@
-var arr = ['1', '3', 'aa'];
-var arry = [];
-
-for (var i = arr.length -1; i >= 0; i--) {
-    arry[arr.length-i-1] = arr[i]
-}
-console.log(arry)
